4cf02ef4c8599dbf3e04812071e0393652c44039
Guilhermo Masid <guilhermomasid@gmail.com>
2023-06-26 12:07:12 -0300

    pyenv shell 3.8.13

--- cgi/upload.py
+++ cgi/upload.py
@@ -13,23 +13,30 @@ def check_file() -> str:
             os.makedirs("./cgi/tmp")
         file_path = "./cgi/tmp/" + path.basename(uploaded_file.filename)
         open(file_path, "wb").write(uploaded_file.file.read())
-        return "The file " + path.basename(uploaded_file.filename) + " was uploaded to " + os.getcwd() + "/cgi/tmp"
+        return os.getcwd() + "/cgi/tmp/" + path.basename(uploaded_file.filename)
     else:
         return "Error: uploading failed"
 
 
-msg = check_file()
+image_path = check_file()
 response_body = [
     "HTTP/1.1 200 OK",
-    "Content-type: image/png;charset=utf-8\n",
+    "Content-type: text/html;charset=utf-8\n",
     "<html>",
     "<head>",
-    "<title>Images | Webserv</title>",
+    "<title>Current Time</title>",
+    '<script src="https://cdn.tailwindcss.com"></script>',
     "</head>",
-    "<body>",
-    f"<h1>{msg}</h1>",
-    "<p>This is the response body.</p>",
+    '<body class="bg-slate-900 h-screen w-screen flex justify-center items-center text-slate-100">',
+    '<div class="flex flex-col space-y-7 flex justify-center items-center p-10">',
+    '<img src="https://private-user-images.githubusercontent.com/54289589/248842975-c2933c77-44dd-4b2b-ba4f-dbbdd3cc0a41.svg?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJrZXkiOiJrZXkxIiwiZXhwIjoxNjg3NzkxNDQzLCJuYmYiOjE2ODc3OTExNDMsInBhdGgiOiIvNTQyODk1ODkvMjQ4ODQyOTc1LWMyOTMzYzc3LTQ0ZGQtNGIyYi1iYTRmLWRiYmRkM2NjMGE0MS5zdmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBSVdOSllBWDRDU1ZFSDUzQSUyRjIwMjMwNjI2JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDIzMDYyNlQxNDUyMjNaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT0zZjJlYzExNzM1YTVjMmYwYTkwODY2YzlhMmVlNmIwYjM0NWJkMWQ3MTRkZmJhOWJiNGVkZjM5ZjM5ZGQyMTYxJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCZhY3Rvcl9pZD0wJmtleV9pZD0wJnJlcG9faWQ9MCJ9.9JDZTA29ECovTWAHtsQL_9323zEBw6_9MXmKvtSezuE" class="w-96" />',
+    '<div class="flex flex-col justify-center items-center space-y-3">',
+    '<h1 class="font-bold text-3xl text-slate-400">Image uplodad to:</h1>',
+    f'<code class="font-bold text-sm bg-slate-800 text-red-500 p-3 rounded-md md:text-xl">{image_path}</code>',
+    "</div>",
+    '<a href="../index.html"class="py-2 px-7 text-center ring-2 ring-purple-500 rounded-md">Ir para página inicial</a>',
+    "</div>",
     "</body>",
-    "</html>"
+    "</html>",
 ]
 [print(i) for i in response_body]
